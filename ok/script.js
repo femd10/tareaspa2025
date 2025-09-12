993f3f014f105b771490a5e5b3eaf6ae014d766c
femd10 <141891008+femd10@users.noreply.github.com>
2025-09-12 13:01:56 -0500

    Add files via upload

---
+++ ok/script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('directorioForm').addEventListener('submit', function() {
+  alert('Datos enviados correctamente. Serás dirigido a la página de resumen.');
+});
